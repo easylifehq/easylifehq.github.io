52a8bab10813fceb44d92d4f919b6c1d3fd3c898
scolety1 <smcolety@gmail.com>
2026-04-02 01:36:36 -0600

    Try 1 to fix the kicking problem

--- js/guard.js
+++ js/guard.js
@@ -1,8 +1,13 @@
 import { auth } from "./firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
+let checked = false;
+
 onAuthStateChanged(auth, (user) => {
+  if (checked) return;
+  checked = true;
+
   if (!user) {
-    window.location.href = "login.html";
+    window.location.replace("https://easylifehq.github.io/login.html");
   }
 });
